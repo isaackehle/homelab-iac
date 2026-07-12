68f257a2605360f0517ccb58a2c89ce7ecfe5beb
Isaac Kehle <isaac@kehle.org>
2026-07-11 23:19:01 -0400

    chore: migrates Nextcloud database from MariaDB to PostgreSQL
    Switches the database service from MariaDB to PostgreSQL:16. This includes updating the database image, adjusting environment variables for connection in both the database and Nextcloud app services, and modifying volume paths for PostgreSQL data persistence.
    
    The Nextcloud app image is also updated to use the `apache` tag, removing a specific major version pin to allow for more flexible updates within the stable branch.

--- nextcloud/init.sh
+++ nextcloud/init.sh
@@ -1,4 +1,4 @@
-mkdir -p /volume1/docker/nextcloud/{app,data,db,ts-state,ts-config}
+mkdir -p /volume1/docker/nextcloud/{app,data,postgres,ts-state,ts-config}
 sudo chown -R $UID:${GROUPS[0]} /volume1/docker/nextcloud
 mkdir -p /volume1/docker/stacks/nextcloud
 sudo chown -R $UID:${GROUPS[0]} /volume1/docker/stacks/nextcloud
